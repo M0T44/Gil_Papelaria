48de714a6b5da48fda5194132211a88312841550
lorraynecheerry <lorraynesabino554@gmail.com>
2024-02-09 14:27:19 -0300

    att

--- gil_papelaria/App.js
+++ gil_papelaria/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Papelaria!!</Text>
+      <Text>Papelaria!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
